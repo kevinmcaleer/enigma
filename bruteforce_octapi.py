cbc76fe4a57de2705ea84f640438ac7ab7b62e8d
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-14 23:10:33 +0000

    updated brute

--- bruteforce_octapi.py
+++ bruteforce_octapi.py
@@ -59,7 +59,7 @@ ring_choice = "1 1 1"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("192.168.2.1", 8002)) # doesn't matter if 8.8.8.8 can't be reached
-cluster = dispy.JobCluster(find_rotor_start, host=s.getsockname()[0], nodes='192.168.2.*')
+cluster = dispy.JobCluster(find_rotor_start, ip_addr==s.getsockname()[0], nodes='192.168.2.*')
 
 jobs = []
 id = 1
